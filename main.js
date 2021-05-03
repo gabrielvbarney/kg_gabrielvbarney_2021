87d5acc5de6d2bbcc47bb66db378e152d8f4e578
Gabriel Barney <gabriel@Gabriels-MacBook-Pro-4.local>
2021-05-02 17:43:48 -0700

    Added Error Checking

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 function main() {
 
-    // need error checking?
-    
     var i, j;
+    var val;
     var phonetic_eq = "";
 
     var num_dict = {
@@ -18,15 +17,28 @@ function main() {
         "9" : "Nine"
     };
 
+    if (process.argv.length < 3) {
+        console.log("usage: node main.js <int1> <int2> ...");
+        process.exit(1); // exit failure
+    }
+
     for (i = 2; i < process.argv.length; i++) {
         for (j = 0; j < process.argv[i].length; j++) {
-            phonetic_eq += num_dict[process.argv[i][j]];
+
+            val = num_dict[process.argv[i][j]];
+            
+            if (val != undefined) {
+                phonetic_eq += val;
+            } else {
+                phonetic_eq += "Nan"; // Not a Number
+            }
+            
         }
         phonetic_eq += ",";
     }
 
-    console.log(phonetic_eq.slice(0, -1)); // remove final comma
+    console.log(phonetic_eq.slice(0, -1)); // Remove final comma
 
 }
 
-main()
+main();
